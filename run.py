c164e4f5b65f7dc6b501728755a194bbf1a19ec8
Bmbaku1 <brmba1@morgan.edu>
2025-12-02 11:45:26 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import create_app
 
-# This creates the Flask application instance using the factory in app/__init__.py
+# This creates the Flask application instance
 app = create_app()
 
 if __name__ == "__main__":
     # This runs the development server
     app.run(debug=True)
+
